d71ab1efc3012825520cbd801c701794ff7e3f10
Serebubi <sbbangg111@gmail.com>
2026-05-18 14:31:56 +0300

    Match Railway default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const root = __dirname;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 const mimeTypes = {
   ".html": "text/html; charset=utf-8",
